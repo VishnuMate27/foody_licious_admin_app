9948339ee0ba80dc6081f78e629855e197ad7808
VishnuMate27 <matevishnu27@gmail.com>
2025-07-31 22:59:34 +0530

    Completed dashboard screen

--- lib/view/post_auth/dashboard.dart
+++ lib/view/post_auth/dashboard.dart
@@ -37,10 +37,10 @@ class _DashboardViewState extends State<DashboardView> {
                 // border: Border.all(color: Color(0xFFE85353), width: 0.5),
                 boxShadow: [
                   BoxShadow(
-                    color: Color(0xFFF4F4F4),
-                    spreadRadius: 4,
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
                     blurRadius: 2,
-                    offset: Offset(0, 4), // changes position of shadow
+                    offset: Offset(0, 1), // changes position of shadow
                   ),
                 ],
               ),
@@ -126,7 +126,338 @@ class _DashboardViewState extends State<DashboardView> {
             ),
           ),
           SizedBox(height: 20.h),
-          
+          GridView(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20.h,
+              crossAxisSpacing: 30.w,
+              childAspectRatio: 1.5,
+            ),
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Add Menu tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.add_circled,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Add Menu",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("All Item Menu tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.eye,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "All Item Menu",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Out For Delivery tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.location,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Out For Delivery",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Feedback tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.chat_bubble,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Feedback",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Profile tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.profile_circled,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Profile",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Money On Holed tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.money_dollar,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Money On Holed",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Create New User tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        // spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.person_add,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Create New User",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Log Out tapped");
+                },
+                child: Container(
+                  width: 150.w,
+                  height: 85.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0C0C0),
+                    borderRadius: BorderRadius.circular(15.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF6CCB94),
+                        blurRadius: 3,
+                        offset: Offset(0, 1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(
+                          CupertinoIcons.add_circled,
+                          size: 30.w,
+                          color: Color(0xFFD13131),
+                        ),
+                        Text(
+                          "Log Out",
+                          style: GoogleFonts.yeonSung(
+                            color: Color(0xFFD13131),
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
